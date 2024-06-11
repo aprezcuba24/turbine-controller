7032a18537a3fb9524fb066db24a3d1a9f56eb32
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-06-10 20:27:31 -0400

    Create a platformio project

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
-int INPUT_WATER_LEVEL = 3;
-int INPUT_RESET = 7;
-int INPUT_STOP_START = 6;
+#include <Arduino.h>
 
-///Security
-int INPUT_SECURITY = 10;
-int OUTPU_ERROR = 9;
-
-//Leds indicators
-int OUTPUT_WATER_HIGH = 13;
-int OUTPUT_WATER_MEDIUN = 12;
-int OUTPUT_WATER_UNDER = 11;
-
-int WATER_HIGH_LEVEL = 1023;
-int WATER_MEDIUM = 818;
-int WATER_UNDER = 306;
-int TIME_TURBINE_LIMIT = 2000;
-
-int OUTPUT_TURBINE = 8;
+#define INPUT_WATER_LEVEL 3
+#define INPUT_RESET 7
+#define INPUT_STOP_START 6
+#define INPUT_SECURITY 10
+#define OUTPUT_ERROR 9
+#define OUTPUT_WATER_HIGH 13
+#define OUTPUT_WATER_MEDIUM 12
+#define OUTPUT_WATER_UNDER 11
+#define WATER_HIGH_LEVEL 1023
+#define WATER_MEDIUM 818
+#define WATER_UNDER 306
+#define TIME_TURBINE_LIMIT 2000
+#define OUTPUT_TURBINE 8
 
 int currentLevel = -1;
 bool inErrorState = false;
 unsigned long startTurbineTime = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -28,11 +25,11 @@ void setup() {
 
   //Indicators leds
   pinMode(OUTPUT_WATER_HIGH, OUTPUT);
-  pinMode(OUTPUT_WATER_MEDIUN, OUTPUT);
+  pinMode(OUTPUT_WATER_MEDIUM, OUTPUT);
   pinMode(OUTPUT_WATER_UNDER, OUTPUT);
 
   pinMode(INPUT_SECURITY, INPUT);
-  pinMode(OUTPU_ERROR, OUTPUT);
+  pinMode(OUTPUT_ERROR, OUTPUT);
   pinMode(OUTPUT_TURBINE, OUTPUT);
   pinMode(INPUT_RESET, INPUT);
 }
@@ -59,7 +56,7 @@ void updateIndicatorsLeds(int currentLevel) {
     digitalWrite(OUTPUT_WATER_UNDER, HIGH);
   }
   if (currentLevel > WATER_UNDER && currentLevel <= WATER_MEDIUM) {
-    digitalWrite(OUTPUT_WATER_MEDIUN, HIGH);
+    digitalWrite(OUTPUT_WATER_MEDIUM, HIGH);
   }
   if (currentLevel > WATER_MEDIUM) {
     digitalWrite(OUTPUT_WATER_HIGH, HIGH);
@@ -110,7 +107,7 @@ void controlTurbine(int level) {
 
 void turnOffLeds() {
   digitalWrite(OUTPUT_WATER_HIGH, LOW);
-  digitalWrite(OUTPUT_WATER_MEDIUN, LOW);
+  digitalWrite(OUTPUT_WATER_MEDIUM, LOW);
   digitalWrite(OUTPUT_WATER_UNDER, LOW);
 }
 
@@ -136,7 +133,7 @@ bool hasError() {
   if (inErrorState) {
     turnOffLeds();
     stopTurbine();
-    ledFlashing(OUTPU_ERROR, 500);
+    ledFlashing(OUTPUT_ERROR, 500);
   }
   Serial.println(inErrorState ? "Error" : "No Error");
   return inErrorState;
